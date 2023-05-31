a6d4cf8bf58a5fa9d8c80ce579bd03245aafb35c
ryanmz1 <ryanmz1@outlook.com>
2023-05-31 22:13:00 +0800

    refactor auth service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,82 +1,31 @@
 import { Injectable } from '@angular/core';
-import { cognito, api } from '../../environments/envConfig';
-import { CognitoUserPool,AuthenticationDetails,CognitoUser } from "amazon-cognito-identity-js";
-// import * as AWSCognito from 'aws-sdk/clients/cognitoidentityserviceprovider';
+import { CognitoAuthService } from "./cognito-auth.service";
+import { MessageService } from "../services/message.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
-
-  private poolData = {
-    UserPoolId: cognito.userPoolId,
-    ClientId: cognito.userPoolClientId,
-  };
-  
-  private userPool = new CognitoUserPool(this.poolData);
+  constructor(private cognitoAuthService: CognitoAuthService) { }
 
   public fetchAuthToken(): Promise<any> {
-    return new Promise((resolve, reject)=> {
-      const cognitoUser = this.getCurrentUser();
-      if (cognitoUser) {
-        cognitoUser.getSession((err: Error, session: any) => {
-          if (err) {
-            reject(err);
-          } else if (!session.isValid()) {
-            resolve(null);
-          } else {
-            resolve(session.getIdToken().getJwtToken());
-          }
-        });
-      } else {
-        resolve(null);
-      }
-    });
+    return this.cognitoAuthService.fetchAuthToken();
   }
   
   public login(email: string, password: string, onSuccess: any, onFailure: any) {
-    const authenticationDetails = new AuthenticationDetails(
-      {
-        Username: this.toUsername(email),
-        Password: password,
-      }
-    );
-  
-    const cognitoUser = this.createCognitoUser(email);
-    cognitoUser.authenticateUser(authenticationDetails, {
-      onSuccess: onSuccess,
-      onFailure: onFailure,
-    });
+    this.cognitoAuthService.login(email, password, onSuccess, onFailure);
   }
   
   public verify(email: string, code: string, onSuccess: any, onFailure: any) {
-    this.createCognitoUser(email).confirmRegistration(
-      code,
-      true,
-      function confirmCallback(err: any, result: any) {
-        if (!err) {
-          onSuccess(result);
-        } else {
-          onFailure(err);
-        }
-      }
-    );
+    
   }
 
-  private createCognitoUser(email: string) {
-    return new CognitoUser({
-      Username: this.toUsername(email),
-      Pool: this.userPool,
-    });
+  public signOut() {
+    this.cognitoAuthService.signOut();
   }
 
-  private toUsername(email: string) {
-    return email.replace('@', '-at-');
+  public signUp(email: string, password: string, onSuccess: any, onFailure: any) {
+    this.cognitoAuthService.signUp(email, password, onSuccess, onFailure);
   }
-
-  private getCurrentUser() {
-    return this.userPool.getCurrentUser();
-}
 }
