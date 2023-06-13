7d40154ff82cc9e29ea7ffdc0e3e9cdb31d984ae
ryanmz1 <ryanmz1@outlook.com>
2023-06-13 21:57:28 +0800

    added user-menu

---
+++ src/app/user-menu/user-menu.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit,Input,ElementRef, ViewChild,Renderer2 } from '@angular/core';
+import { Router } from "@angular/router";
+import { EventBusService } from "../services/event-bus.service";
+import { AuthService } from '../services/auth.service';
+
+@Component({
+  selector: 'app-user-menu',
+  templateUrl: './user-menu.component.html',
+  styleUrls: ['./user-menu.component.css']
+})
+export class UserMenuComponent implements OnInit {
+  @ViewChild('loginLink',{static:true})
+  private loginLink!: ElementRef
+  @ViewChild('signoutLink',{static:true})
+  private signoutLink!: ElementRef
+
+  constructor(private eventBusService: EventBusService,
+    private render: Renderer2,
+    private authService: AuthService,
+    private router: Router) { }
+
+  ngOnInit(): void {
+    this.eventBusService.on('toggleLogin', (payload: any) => {
+      if (payload.login) {
+        this.removeLogin();
+      } else if (!payload.login) {
+        this.removeSignOut();
+      }
+    });
+  }
+
+  public removeLogin() {
+    this.render.setStyle(this.loginLink.nativeElement, 'display', 'none');
+    this.render.removeStyle(this.signoutLink.nativeElement, 'display');
+  }
+
+  public removeSignOut() {
+    this.render.setStyle(this.signoutLink.nativeElement, 'display', 'none');
+    this.render.removeStyle(this.loginLink.nativeElement, 'display');
+  }
+
+  public handleLogin() {
+    // this.eventBusService.emit({type:'loginModal',payload:{show:true}});
+    this.router.navigateByUrl('/auth/login');
+  }
+
+  public handleSignout() {
+    this.authService.signOut();
+  }
+
+}
