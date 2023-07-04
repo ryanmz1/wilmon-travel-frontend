d6b124168707bb09db9e5a9c5d93b9bfdc27bda0
ryanmz1 <ryanmz1@outlook.com>
2023-07-04 21:01:17 +0800

    style(rename):wm-travel-api

--- src/app/services/wm-travel-api.service.spec.ts
+++ src/app/services/wm-travel-api.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { WmTravelService } from './wm-travel.service';
+import { WmTravelService } from './wm-travel-api.service';
 
 describe('WmTravelService', () => {
   let service: WmTravelService;
